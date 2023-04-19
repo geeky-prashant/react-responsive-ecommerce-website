d30fd78b60e93d65a63fab87d07fb963d8dfab49
Prashant Bhardwaj <prashant.b@zengatv.com>
2023-04-19 21:21:14 +0530

    Categories Updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,20 +2,16 @@ import { useEffect, useContext } from "react";
 import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
-import { Context } from "../../utils/context";
 import Products from "./../Products/Products";
 
 const Home = () => {
-  const { categories, setCategories, products, setProducts } =
-    useContext(Context);
-
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category categories={categories} />
-          <Products products={products} headingText="Popular Products" />
+          <Category />
+          <Products headingText="Popular Products" />
         </div>
       </div>
     </div>
